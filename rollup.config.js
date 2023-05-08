5f87c64d834f5c78260227b4994a542839bcba92
Cris van Pelt <cris@cl.ly>
2023-05-08 10:23:00 -0500

    types

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,30 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import { minify } from 'rollup-plugin-esbuild';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      name: 'noodly_fsm',
-      file: 'dist/index.min.js',
-      format: 'es',
-      plugins: [minify({})],
-      sourcemap: true,
-    },
-    {
-      file: 'dist/index.js',
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript()],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        name: 'noodly_fsm',
+        file: 'dist/index.min.js',
+        format: 'es',
+        plugins: [minify({})],
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.js',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
+    input: ['src/fsm.ts'],
+    output: [{ file: 'dist/fsm.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
