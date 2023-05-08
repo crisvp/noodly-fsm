6db10c8bb62b5daf05d8cdada4e32c240f8c73d9
Cris van Pelt <cris@cl.ly>
2023-05-08 10:00:10 -0500

    build

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
+import { minify } from 'rollup-plugin-esbuild';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      name: 'noodly_fsm',
+      file: 'dist/index.min.js',
+      format: 'es',
+      plugins: [minify({})],
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript()],
+};
